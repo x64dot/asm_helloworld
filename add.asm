42b3dad2bbdb9a952dfcf94fc9db4bb3484351a4
x64dot <142705403+x64dot@users.noreply.github.com>
2024-08-05 18:31:56 -0400

    Update add.asm

--- add.asm
+++ add.asm
@@ -2,6 +2,7 @@ section .data
       num1 dq 5
       num2 dq 5
       result dq 0
+      text db "Check the return to get the sum", 10
 
 section .text
       global _start
@@ -13,6 +14,14 @@ _start:
       add rax,rbx
        
       mov [result],rax        
+      
+      ; sys_write
+      
+      mov rax,1
+      mov rdi,1
+      mov rsi,text
+      mov rdx,35
+      syscall
 
       ; sys_exit and returning the sum of 5+5 
      
