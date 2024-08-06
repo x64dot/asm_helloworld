5a62bd40ca20b83ef5f65367ee5f73cbf1ae87b9
x64dot <142705403+x64dot@users.noreply.github.com>
2024-08-05 23:54:02 -0400

    Create outputname.asm

---
+++ outputname.asm
@@ -0,0 +1,32 @@
+section .data
+text db "Enter your name: "
+
+section .bss
+buffer resb 128
+
+section .text
+      global _start
+_start:
+      mov rax,1
+      mov rdi,1
+      mov rsi,text
+      mov rdx,17
+      syscall
+     
+      mov rax,0
+      mov rdi,0
+      mov rsi,buffer
+      mov rdx,128
+      syscall
+     
+      mov rax,1
+      mov rdi,1
+      mov rsi,buffer
+      mov rdx,128
+      syscall   
+   
+       
+  
+      mov rax,60
+      mov rdi,0
+      syscall
